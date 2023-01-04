2381607c397ac974bd88be7103bfb9dd63c57d0e
Ellie OConnor <ellieoconnor3114@gmail.com>
2023-01-04 11:25:26 -0500

    Level 7 kata completed

---
+++ 7kyu/ListFiltering.js
@@ -0,0 +1,10 @@
+//INSTRUCTIONS
+/*
+In this kata you will create a function that takes a list of non-negative integers and string and returns a new list with the string
+filtered out.
+EXAMPLE
+filter_list([1,2,'a','b']) == [1,2] */
+
+function filter_list(l){
+    return l.filter((x) => typeof x === "number")
+}
