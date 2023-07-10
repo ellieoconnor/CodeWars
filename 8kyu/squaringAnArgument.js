06e11c5679abd6a64e0cad8404e823e7e235876f
Ellie O'Connor <87724044+ellieoconnor@users.noreply.github.com>
2023-07-10 10:00:25 -0400

    Add files via upload

---
+++ 8kyu/squaringAnArgument.js
@@ -0,0 +1,11 @@
+//FUNCTION 2 - SQUARING AN ARGUMENT
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+function square(n){
+    return n**2;
+}
+
+//ALTERNATIVES
+const square = (n) => n*n;
---
+++ 8kyu/welcome!.js
@@ -0,0 +1,36 @@
+//WELCOME!
+/*
+Your start-up's BA has told marketing that your website has a large audience is Scandinavia and 
+surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own
+language. Luckily you already use an API that detects the user's location, so this is an easy win.
+
+Think of a way to store the lanaguage as a database (e.g. an object). The languages are listed below so
+you can copy and share.
+
+Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a 
+greeting - If you have it in your database. It should default to English if the language is not in the
+datbase, or in the even of an invalid input.
+*/
+
+function greet(language){
+    const welcomes = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    };
+    return language in welcomes ? welcome[language] : welcome.english;
+}
